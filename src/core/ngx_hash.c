724b12f8bef6a605e749ddeaf24c1702068e98be
liuyunbin <liuyunbin6537@gmail.com>
2018-02-05 13:50:57 +0800

    change 2 to sizeof(u_short)

--- src/core/ngx_hash.c
+++ src/core/ngx_hash.c
@@ -246,7 +246,7 @@ ngx_hash_find_combined(ngx_hash_combined_t *hash, ngx_uint_t key, u_char *name,
 
 
 #define NGX_HASH_ELT_SIZE(name)                                               \
-    (sizeof(void *) + ngx_align((name)->key.len + 2, sizeof(void *)))
+    (sizeof(void *) + ngx_align((name)->key.len + sizeof(u_short), sizeof(void *)))
 
 ngx_int_t
 ngx_hash_init(ngx_hash_init_t *hinit, ngx_hash_key_t *names, ngx_uint_t nelts)
